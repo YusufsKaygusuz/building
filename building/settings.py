dda5ba7c3cfd28c9b17912a2e9fd12af0d1bc005
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2023-12-29 22:40:22 +0300

    arranged

--- building/settings.py
+++ building/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$rryds%zp#($kd20(14-0xyx@a@5+5@vr9z$-c1e5)*+lza@f(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
