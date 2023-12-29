377480588b8d4ea998c094b65098de1ae569af80
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2023-12-29 22:49:15 +0300

    getenv added

--- building/settings.py
+++ building/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$rryds%zp#($kd20(14-0xyx@a@5+5@vr9z$-c1e5)*+lza@f('
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['severbuilding-a85fb2b21768.herokuapp.com']
 
 
 # Application definition
