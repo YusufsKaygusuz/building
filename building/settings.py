43557dfc0737b55b5c95d7adf20677575d4427b4
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2024-01-06 17:27:01 +0300

    heroku postgresql setting

--- building/settings.py
+++ building/settings.py
@@ -78,12 +78,25 @@ WSGI_APPLICATION = 'building.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': getenv('DBNAME'),
+            'USER': getenv('DBUSER'),
+            'PASSWORD': getenv('DBPASSWORD'),
+            'HOST': getenv('DBHOST'),
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
