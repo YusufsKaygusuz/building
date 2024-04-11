c6e8a9c768e002576e2eb24a4ee102aa3daed540
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2024-04-11 16:21:12 +0300

    admin url has been changed

--- building/urls.py
+++ building/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('construction.urls')),
-    path('admin/', admin.site.urls),
+    path('secure-admin/', admin.site.urls),
 ]
